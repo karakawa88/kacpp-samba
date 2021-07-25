06ae337468418456715fc568c3af47501e53c1a0
karaspdev <kagalpan@kacpp.xyz>
2021-07-25 15:20:45 +0900

    docker-compose.shでdocker-composeで起動後コンテナーで初期化処理を行うスクリプト追加。
    スクリプト名はpostexec.shでdocker-compose.shがコンテナ起動後実行する。
    Samba用環境変数を用意するためkacpp-samba.srcをdocker-compose用に作成。
    samba-init.shでSambaユーザーパスワードTDBファイルをコピーして初期化する処理を追加したが
    反映されなかったのでコメントアウトした。

---
+++ postexec.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker exec kacpp-samba "/usr/local/sh/system/samba-init.sh"
+
+exit 0
--- sh/system/samba-init.sh
+++ sh/system/samba-init.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# マウントポイントのディレクトリを作成
+mp_list="/usr/local/etc/samba/mp.txt"
+if [[ -r ${mp_list} ]]
+then
+    /usr/local/sh/system/mp.sh ${mp_list}
+fi
+
+# ログインユーザーとsambaユーザーの作成
+users_list="/usr/local/etc/samba/users_list.txt"
+if [[ -r ${users_list} ]]
+then
+    /usr/local/sh/system/user_add.sh ${users_list}
+fi
+
+# pdbeditのパスワードファイルの設定
+SAMBA_TDB_DIR=${SAMBA_HOME}/var/lib/samba/private
+# SAMBA_TDB_FILE=${TDB_DIR}/passdb.tdb
+TDB_DIR="/usr/local/etc/samba/private"
+if [[ -d ${TDB_DIR} ]]
+then
+    cp -rf ${TDB_DIR} ${SAMBA_TDB_DIR}
+fi
+
+sleep 2
 systemctl daemon-reload
 systemctl enable smbd
 systemctl enable nmbd
